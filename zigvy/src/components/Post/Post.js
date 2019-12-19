7a6009da62eb9d36e949357e6ed41961505f826f
wolfag <wolfag91@gmail.com>
2019-12-19 23:29:12 +0700

    add ui delete, edit post

--- zigvy/src/components/Post/Post.js
+++ zigvy/src/components/Post/Post.js
@@ -1,28 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
-import { Typography, Tag } from "antd";
+import { Typography, Tag, Card, Icon, Modal } from "antd";
 import { isEmpty } from "lodash";
 
 import Constant from "common/constant";
 import { CommentList } from "components/Comment";
 
 const { Title, Paragraph } = Typography;
+const { confirm } = Modal;
 
 function Post(props) {
-  const { title, author, createdAt, tags, content } = props;
+  const {
+    id,
+    title,
+    author,
+    createdAt,
+    tags,
+    content,
+    onDeletePost,
+    onEditPost
+  } = props;
+
+  const _showDeleteConfirm = () => {
+    confirm({
+      title: "Are you sure delete this post?",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        onDeletePost(id);
+      },
+      onCancel() {
+        console.log("Cancel");
+      }
+    });
+  };
+
   return (
-    <div
+    <Card
       style={{
-        margin: 30,
         width: "100%"
       }}
+      extra={
+        <span
+          style={{
+            width: 50,
+            display: "flex",
+            justifyContent: "space-between"
+          }}
+        >
+          <Icon type="edit" onClick={onEditPost} />
+          <Icon type="close" onClick={_showDeleteConfirm} />
+        </span>
+      }
+      title={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: 20
+          }}
+        >
+          <Title level={2}>{title}</Title>
+        </div>
+      }
     >
-      <div
-        style={{ display: "flex", justifyContent: "center", marginBottom: 20 }}
-      >
-        <Title level={2}>{title}</Title>
-      </div>
       <div
         style={{
           display: "flex",
@@ -49,18 +92,22 @@ function Post(props) {
         </Paragraph>
       </div>
       <CommentList />
-    </div>
+    </Card>
   );
 }
 
 Post.propTypes = {
+  id: PropTypes.number,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   createdAt: PropTypes.number.isRequired,
   tags: PropTypes.array,
-  content: PropTypes.string.isRequired
+  content: PropTypes.string.isRequired,
+  onDeletePost: PropTypes.func,
+  onEditPost: PropTypes.func
 };
 Post.defaultProps = {
+  id: 1,
   title: "Post 1",
   author: "Author 1",
   createdAt: 1576506719083,
@@ -70,7 +117,13 @@ Post.defaultProps = {
   a design language for background applications, is refined by Ant UED Team. Ant Design, a
   design language for background applications, is refined by Ant UED Team. Ant Design, a design
   language for background applications, is refined by Ant UED Team. Ant Design, a design
-  language for background applications, is refined by Ant UED Team.`
+  language for background applications, is refined by Ant UED Team.`,
+  onDeletePost: () => {
+    alert("onDeletePost");
+  },
+  onEditPost: () => {
+    alert("onEditPost");
+  }
 };
 
 export default Post;
